0f395874046feb75159c387432d1b70541cbd558
Navnath Kadam <navnathkadam284@gmail.com>
2026-02-08 19:21:05 +0530

    feat: added sample option page for testing

---
+++ src/options/options.tsx
@@ -0,0 +1,13 @@
+function Options() {
+    return (
+        <>
+            <h1>
+                Options Page  
+            </h1>
+            
+            {/* Here Option Component will be mounted */}
+        </>
+    )
+}
+
+export default Options;
